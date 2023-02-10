50ce085e73970adc8f0159cfe5e0017a06e0bfaf
Robert Kasza <kaszarobi.27@gmail.com>
2023-02-10 20:45:15 +0100

    build dotnet apps with lando

---
+++ Build .NET core app/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Display the number of command line arguments.
+        Console.WriteLine("Number of arguments: {0}", args.Length);
+
+        for (int i = 0; i < args.Length; i++)
+        {
+            Console.WriteLine("{0}. argument: {1}", i, args[i]);
+        }
+    }
+}
